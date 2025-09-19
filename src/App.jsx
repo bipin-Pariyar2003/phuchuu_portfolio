e3e8dd54975abaac17f608ec8ae6273ae97b1f47
bipin-Pariyar2003 <163013814+bipin-Pariyar2003@users.noreply.github.com>
2025-09-19 20:08:08 +0545

    fixed margin issue on top

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,32 @@ import About from "./views/About";
 import Contact from "./views/Contact";
 import Blog from "./views/Blog";
 import Projects from "./views/Projects";
+import { Box, CssBaseline } from "@mui/material";
+import "./App.css";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/projects" element={<Projects />} />
-        </Routes>
-      </BrowserRouter>
+      <CssBaseline />
+      <Box
+        sx={{
+          minHeight: "100vh",
+          background: "linear-gradient(135deg, #0f2027, #203a43, #2c5364)",
+          m: 0,
+          p: 0,
+        }}
+      >
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/projects" element={<Projects />} />
+          </Routes>
+        </BrowserRouter>
+      </Box>
     </>
   );
 }
