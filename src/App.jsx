8afa145834d69e02fa0bcc2ac803b656375f5f0d
bipin-Pariyar2003 <163013814+bipin-Pariyar2003@users.noreply.github.com>
2025-09-19 18:05:55 +0545

    router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./views/Home";
 
 function App() {
-  return <>hello</>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
