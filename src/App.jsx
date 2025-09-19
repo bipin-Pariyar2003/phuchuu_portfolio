bcee233a09fc97ac508d7f2ded88c97c46abd116
bipin-Pariyar2003 <163013814+bipin-Pariyar2003@users.noreply.github.com>
2025-09-19 19:50:30 +0545

    created pages and routes for about, contact, blog and projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./views/Home";
+import About from "./views/About";
+import Contact from "./views/Contact";
+import Blog from "./views/Blog";
+import Projects from "./views/Projects";
 
 function App() {
   return (
@@ -8,6 +11,11 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/projects" element={<Projects />} />
         </Routes>
       </BrowserRouter>
     </>
