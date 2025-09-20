ce8dcacd88e1d92ecc6debf8862a99e49bf9797d
bipin-Pariyar2003 <163013814+bipin-Pariyar2003@users.noreply.github.com>
2025-09-20 14:58:30 +0545

    confugured pwa support

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Phuchuu Portfolio",
+        short_name: "Portfolio",
+        description: "My portfolio as a Progressive Web App",
+        theme_color: "#000000",
+        background_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
